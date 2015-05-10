56858cd98aa0331d3c46d59c031465d121e40908
moonc <allenjgk@gmail.com>
2015-05-09 19:10:08 -0700

    Create Quiz.java
    The quiz class for the digestive system in the SystemsAndOrgans program

---
+++ Quiz.java
@@ -0,0 +1,45 @@
+import javax.swing.*;//import swing 
+import java.awt.event.*;//import awt.event.*
+import java.awt.*;//import awt.event.*
+public class Quiz extends JPanel implements ActionListener{ 
+	public Quiz(){//constructor method
+		/*create JButtonGroup 1 - FIrst Question 
+		*JRadioButton for choice 1
+		*JRadioButton for choice 2
+		*JRadioButton for choice 3
+		*/
+		/*create JButtonGroup 2 - Second QUestion
+		*JRadioButton for choice 1
+		*JRadioButton for choice 2
+		*JRadioButton for choice 3
+		 */
+		/*create JButtonGroup 3 - Third Question
+		*JRadioButton for choice 1
+		*JRadioButton for choice 2
+		*JRadioButton for choice 3
+		 */
+		/*create JButtonGroup 4 - FOurth Question
+		*JRadioButton for choice 1
+		*JRadioButton for choice 2
+		*JRadioButton for choice 3
+		 */
+		//JTextField(s) for short answer question(s)
+
+	}
+	public void actionPerformed(ActionEvent e){
+		//when "Go Back" button is clicked, go to the Start menu
+		
+		}
+}	
+
+
+
+/*MLA CITATIONS:
+ * DIgestive image: Digestive System. Digital image.Pintrest- Front Porch Party - Home School. 19 Mar. 2014. Web.
+ * Endocrine : Endocrine Glands Diagram Labeled. Digital image. Imgbuddy.Web.
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
